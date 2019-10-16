9c34e13f10732f0a210cec5f9da0e9b6a7d038eb
asovic <50458542+asovic@users.noreply.github.com>
2019-10-16 11:08:34 +0200

    Update data_logger.py

--- data_logger.py
+++ data_logger.py
@@ -10,13 +10,13 @@ def GetData():
     sensor = Adafruit_DHT.DHT22
     pin = 4
     temp, hum = Adafruit_DHT.read_retry(sensor,pin)
-    #if hum is not None and temp is not None:
-    if hum <= 100.0 and hum >= 0.0 and temp <= 100.0 and temp >= -50.0:
-        hum = round(hum,1)
-        temp = round(temp,1)
-        return hum,temp
-#        else:
-#            print('Bad reading at {}'.format(datetime.datetime.now()))
+    
+    #if hum <= 100.0 and hum >= 0.0 and temp <= 100.0 and temp >= -50.0:
+    hum = round(hum,1)
+    temp = round(temp,1)
+    return hum, temp
+    #else:
+        #print('Bad reading at {}'.format(datetime.datetime.now()))
 
 def LogData(temp,hum):
     conn = sqlite3.connect(database)
